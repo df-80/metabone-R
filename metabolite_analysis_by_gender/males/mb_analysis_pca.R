358c7815c79f36cb7723dfec852b2e0c8bcab5d8
donald-80 <donald.friggieri@gmail.com>
2022-12-29 10:07:29 +0100

    PCA by gender.

--- metabolite_analysis_by_gender/males/mb_analysis_pca.R
+++ metabolite_analysis_by_gender/males/mb_analysis_pca.R
@@ -1,54 +1,27 @@
 ## IMPORTANT NOTE #######################################################################################
-## Execute mb_data_filtering.R - From line 1 to 51 ######################################################
-## To see PCA before batch effect correction, execute from line 1 to line 44 ############################
+## Execute mb_data_filtering.R - From line 1 to 63 ######################################################
+## To see PCA before batch effect correction, execute from line 1 to line 45 ############################
 #########################################################################################################
 
 # Load necessary libraries
 pacman::p_load(caret, ggplot2)
 
-attach(ds)
+# Filter by Gender - get Males only
+ds <- ds[ds$Gender == 'Male',]
+cat("Rows:", nrow(ds),", Columns:", ncol(ds),"\n")
+
+# Save the non-metabolite variables
+nmv_ds <- ds[ , 1:22]
+ds <- ds[ , 23:ncol(ds)]
 
 ## Scale data - Use caret library to MinMax scale all columns
 process <- preProcess(as.data.frame(ds), method = "range")
 norm_scale <- predict(process, as.data.frame(ds))
 
-## Batch-Effect PCA #####################################################################################
-## Insert Batch column in ds dataframe and concatenate the scaled dataset
-ds <- data.frame(dataset['Batch'], norm_scale)
-## Change Batch to categorical variable
-ds$Batch <- factor(ds$Batch, levels=c(0,1), labels=c("Batch1", "Batch2"))
-
-## PCA experiment - Get dataset to work on, i.e. remove Batch which will be our variable
-pca_dataset <- ds[ , 2:163]
-
-## PCA ##################################################################################################
-pca <- prcomp(pca_dataset)
-pca
-
-## returns the scree plot showing the variances explained by the principal ## components versus the principal component number
-plot(pca, type = "l")
-
-# Get summary of the pca variable
-summary(pca)  # Cutoff after 3
-
-## Plot PCA #############################################################################################
-## 2D PCA ###############################################################################################
-library(ggfortify)
-pca_dataset$Batch <- ds$Batch
-autoplot(pca, data = pca_dataset, colour = 'Batch', label = FALSE)
-
-library(ggbiplot)
-g1 <- ggbiplot(pca, obs.scale = 1, var.scale = 1, groups = ds$Batch, ellipse = TRUE, circle = TRUE)
-g1 <- g1 + scale_color_discrete(name = '')
-g1 <- g1 + theme(legend.direction = 'horizontal', legend.position = 'top')
-g1
-#########################################################################################################
 
 ## Fractures PCA ########################################################################################
 ## Insert Status Fractures column in ds dataframe
-ds <- data.frame(dataset['Status_Fractures'], norm_scale)
-## Change Batch to categorical variable
-ds$Status_Fractures <- factor(ds$Status_Fractures, levels=c(0,1), labels=c("No", "Yes"))
+ds <- data.frame(nmv_ds['Status_Fractures'], norm_scale)
 
 ## PCA experiment - Get dataset to work on, i.e. remove Status_Fractures which will be our variable
 pca_dataset <- ds[ , 2:163]
@@ -78,9 +51,7 @@ g1
 
 ## BMD PCA ##############################################################################################
 ## Insert Status BMD column in ds dataframe
-ds <- data.frame(dataset['Status_BMD'], norm_scale)
-## Change Status_BMD to categorical variable
-ds$Status_BMD <- factor(ds$Status_BMD, levels=c(0,1,2), labels=c("Normal", "Osteopenic", "Osteoporotic"))
+ds <- data.frame(nmv_ds['Status_BMD'], norm_scale)
 
 ## PCA experiment - Get dataset to work on, i.e. remove Status_BMD which will be our variable
 pca_dataset <- ds[ , 2:163]
@@ -110,9 +81,7 @@ g1
 
 ## Lumbar Spine PCA #####################################################################################
 ## Insert LS Status column in ds dataframe
-ds <- data.frame(dataset['LS_status'], norm_scale)
-## Change LS_Status to categorical variable
-ds$LS_status <- factor(ds$LS_status, levels=c(0,1,2), labels=c("Normal", "Osteopenic", "Osteoporotic"))
+ds <- data.frame(nmv_ds['LS_status'], norm_scale)
 
 ## PCA experiment - Get dataset to work on, i.e. remove LS_Status which will be our variable
 pca_dataset <- ds[ , 2:163]
@@ -142,9 +111,7 @@ g1
 
 ## Femoral Neck PCA #####################################################################################
 ## Insert FN Status column in ds dataframe
-ds <- data.frame(dataset['FN_status'], norm_scale)
-## Change FN_Status to categorical variable
-ds$FN_status <- factor(ds$FN_status, levels=c(0,1,2), labels=c("Normal", "Osteopenic", "Osteoporotic"))
+ds <- data.frame(nmv_ds['FN_status'], norm_scale)
 
 ## PCA experiment - Get dataset to work on, i.e. remove FN_Status which will be our variable
 pca_dataset <- ds[ , 2:163]
@@ -174,9 +141,7 @@ g1
 
 ## Total Hip PCA ########################################################################################
 ## Insert FN Status column in ds dataframe
-ds <- data.frame(dataset['TH_status'], norm_scale)
-## Change TH_Status to categorical variable
-ds$TH_status <- factor(ds$TH_status, levels=c(0,1,2), labels=c("Normal", "Osteopenic", "Osteoporotic"))
+ds <- data.frame(nmv_ds['TH_status'], norm_scale)
 
 ## PCA experiment - Get dataset to work on, i.e. remove TH_Status which will be our variable
 pca_dataset <- ds[ , 2:163]
@@ -208,21 +173,25 @@ g1
 ## Execute PCA on Fractures and BMD according to p-values < 0.05 from exploratory outcomes ##############
 ## Fractures PCA ########################################################################################
 ## Insert Status Fractures column in ds dataframe
-ds <- data.frame(dataset['Status_Fractures'], norm_scale)
-## Change Batch to categorical variable
-ds$Status_Fractures <- factor(ds$Status_Fractures, levels=c(0,1), labels=c("No", "Yes"))
+ds <- data.frame(nmv_ds['Status_Fractures'], norm_scale)
 
 ## PCA experiment - Set those variables which have shown a p-value < 0.05 with mann-whitney
-# pca_dataset <- ds[ , c("Acetate", "Lactate", "Gly", "DHA", "GlycA", "Omega.3", "Unsaturation", "Ala", "Phosphoglyc",
-#                        "Cholines","XL.HDL.L","M.HDL.FC","HDLsize","XL.HDL.PL","LA","L.HDL.PL","M.HDL.PL","HDL.PL")]
-## PCA experiment - Set those variable with p-value < 0.05 and are on opposite sides of experiment above
-pca_dataset <- ds[ , c("Gly", "Omega.3", "Ala","M.HDL.FC","L.HDL.PL","M.HDL.PL","HDL.PL")]
+pca_dataset <- ds[ , c("Gly", "Total.CE", "Sphingomyelins", "Total.C", "Total.FC", "ApoB", "Total.L", "Omega.6")]
+# pca_dataset <- ds[ , c("Gly", "L.LDL.FC", "LDL.FC", "M.LDL.FC", "Clinical.LDL.C", "Total.CE", "L.LDL.PL", "L.LDL.C", "L.LDL.CE", "S.LDL.FC",
+#                        "Sphingomyelins", "LDL.PL", "L.LDL.L", "LDL.C", "S.LDL.C", "M.VLDL.CE", "S.LDL.P", "Total.C", "LDL.CE", "LDL.L",
+#                        "IDL.C", "IDL.FC", "L.LDL.P", "M.LDL.C", "S.LDL.L", "S.LDL.CE", "Total.FC", "S.LDL.PL", "IDL.CE", "M.LDL.PL",
+#                        "non.HDL.C", "M.VLDL.C", "M.LDL.L", "M.LDL.CE", "IDL.L", "IDL.PL", "LDL.P", "IDL.P", "ApoB", "Remnant.C", "M.HDL.TG",
+#                        "M.VLDL.FC", "S.VLDL.FC", "Total.L", "Omega.6", "HDL.TG", "M.LDL.P")]
+# pca_dataset <- ds[ , c("Gly", "LDL.FC", "Clinical.LDL.C", "Total.CE", "Sphingomyelins", "LDL.PL", "LDL.C", "Total.C", "LDL.CE", "LDL.L",
+#                        "IDL.C", "IDL.FC", "Total.FC", "IDL.CE", "non.HDL.C", "IDL.L", "IDL.PL", "LDL.P", "IDL.P", "ApoB", "Remnant.C",
+#                        "Total.L", "Omega.6", "HDL.TG")]
 
 ## PCA ##################################################################################################
 pca <- prcomp(pca_dataset)
 pca
 
-## returns the scree plot showing the variances explained by the principal ## components versus the principal component number
+## returns the scree plot showing the variances explained by the principal components
+## versus the principal component number
 plot(pca, type = "l")
 
 # Get summary of the pca variable
@@ -242,15 +211,11 @@ g1
 #########################################################################################################
 
 ## BMD PCA ##############################################################################################
-## Insert Status BMB column in ds dataframe
-ds <- data.frame(dataset['Status_BMD'], norm_scale)
-## Change Status_BMD to categorical variable
-ds$Status_BMD <- factor(ds$Status_BMD, levels=c(0,1,2), labels=c("Normal", "Osteopenic", "Osteoporotic"))
+## Insert Status BMD column in ds dataframe
+ds <- data.frame(nmv_ds['Status_BMD'], norm_scale)
 
-## PCA experiment - Get dataset to work on, i.e. remove Status_BMD which will be our variable
-# pca_dataset <- ds[ , c("His", "Val", "Leu", "TotalBCAA", "Ile", "Phe", "Tyr")]
-## PCA experiment with opposing effects on PCA
-pca_dataset <- ds[ , c("His", "Gly", "Ala", "Cholines","Phosphoglyc")]
+## PCA experiment - Set those variables which have shown a p-value < 0.05 with kruskall-wallis
+pca_dataset <- ds[ , c("Sphingomyelins", "Gly", "Total.C", "ApoB", "Total.CE", "His", "Total.L")]
 
 ## PCA ##################################################################################################
 pca <- prcomp(pca_dataset)
@@ -270,6 +235,36 @@ autoplot(pca, data = pca_dataset, colour = 'Status_BMD', label = FALSE)
 
 library(ggbiplot)
 g1 <- ggbiplot(pca, obs.scale = 1, var.scale = 1, groups = ds$Status_BMD, ellipse = TRUE, circle = TRUE)
+g1 <- g1 + scale_color_discrete(name = '')
+g1 <- g1 + theme(legend.direction = 'horizontal', legend.position = 'top')
+g1
+#########################################################################################################
+
+## Total Hip BMD PCA ####################################################################################
+## Insert Status BMD column in ds dataframe
+ds <- data.frame(nmv_ds['TH_status'], norm_scale)
+
+## PCA experiment - Set those variables which have shown a p-value < 0.05 with kruskall-wallis
+pca_dataset <- ds[ , c("Gly", "Acetoacetate", "Acetone", "Sphingomyelins","LDL.PL")]
+
+## PCA ##################################################################################################
+pca <- prcomp(pca_dataset)
+pca
+
+## returns the scree plot showing the variances explained by the principal ## components versus the principal component number
+plot(pca, type = "l")
+
+# Get summary of the pca variable
+summary(pca)  # Cutoff after 2
+
+## Plot PCA #############################################################################################
+## 2D PCA ###############################################################################################
+library(ggfortify)
+pca_dataset$TH_status <- ds$TH_status
+autoplot(pca, data = pca_dataset, colour = 'TH_status', label = FALSE)
+
+library(ggbiplot)
+g1 <- ggbiplot(pca, obs.scale = 1, var.scale = 1, groups = ds$TH_status, ellipse = TRUE, circle = TRUE)
 g1 <- g1 + scale_color_discrete(name = '')
 g1 <- g1 + theme(legend.direction = 'horizontal', legend.position = 'top')
 g1
