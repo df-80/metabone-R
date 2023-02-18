89aef6b61e9bb98f8bebea9ac13e75828bd545ac
donald-80 <donald.friggieri@gmail.com>
2023-02-18 16:51:46 +0100

    Random forest adapted for males.

--- metabolite_analysis_by_gender/males/mb_analysis_random_forest.R
+++ metabolite_analysis_by_gender/males/mb_analysis_random_forest.R
@@ -23,24 +23,40 @@ ds_metabolome <- t(removeBatchEffect(t(ds_metabolome), batch = dataset$Batch))
 ###################################################################################################
 
 # Merge status fractures to the metabolome data
-ds <- data.frame(dataset$Status_Fractures, ds_metabolome)
-ds$dataset.Status_Fractures <- factor(ds$dataset.Status_Fractures, levels=c(0,1), labels = c("No", "Yes"))
-names(ds)[names(ds) == "dataset.Status_Fractures"] <- "Status_Fractures"
+ds_metabolome <- data.frame(dataset$Gender, dataset$Status_Fractures, dataset$Age, dataset$Alcohol, dataset$BMI, ds)
+ds_metabolome$dataset.Status_Fractures <- factor(ds_metabolome$dataset.Status_Fractures, levels=c(0,1), labels = c("No", "Yes"))
+names(ds_metabolome)[names(ds_metabolome) == "dataset.Gender"] <- "Gender"
+names(ds_metabolome)[names(ds_metabolome) == "dataset.Status_Fractures"] <- "Status_Fractures"
+names(ds_metabolome)[names(ds_metabolome) == "dataset.Age"] <- "Age"
+names(ds_metabolome)[names(ds_metabolome) == "dataset.Alcohol"] <- "Alcohol"
+names(ds_metabolome)[names(ds_metabolome) == "dataset.BMI"] <- "BMI"
+
+# Filter by Gender - get Males only
+ds_metabolome <- ds_metabolome[ds_metabolome$Gender == 0, ]
+
+# Drop Gender columns as all values are 0.
+ds_metabolome <- ds_metabolome[-1]
 
 # View dataset
-View(ds)
+View(ds_metabolome)
 
 # Show factors in the dataset
-str(ds)
+str(ds_metabolome)
 
 ## Start building a random forest
 set.seed(42)
 
-## Impute any missing values in the training set using proximities
-ds.imputed <- rfImpute(Status_Fractures ~ ., data = ds, iter = 6)
+## Impute any missing values in the training set using proximities - No need to impute for Males
+#ds.imputed <- rfImpute(Status_Fractures ~ ., data = ds_metabolome, iter = 6)
 
 ## Get random forest model with Status Fractures as the response variable
-model <- randomForest(Status_Fractures ~ ., data=ds.imputed, proximity=TRUE)
+#model <- randomForest(Status_Fractures ~ ., data=ds_metabolome, proximity=TRUE)
+model <- randomForest(
+  Status_Fractures ~ Gly + L.LDL.FC + LDL.FC + M.LDL.FC + Clinical.LDL.C + Total.CE + L.LDL.PL + L.LDL.C + L.LDL.CE + S.LDL.FC +
+    Sphingomyelins + LDL.PL + L.LDL.L + LDL.C + S.LDL.C + M.VLDL.CE + S.LDL.P + Total.C + LDL.CE + LDL.L + IDL.C + IDL.FC + L.LDL.P +
+    M.LDL.C + S.LDL.L + S.LDL.CE + Total.FC + S.LDL.PL + IDL.CE + M.LDL.PL + non.HDL.C + M.VLDL.C + M.LDL.L + M.LDL.CE + IDL.L + IDL.PL +
+    LDL.P + IDL.P + ApoB + Remnant.C + M.HDL.TG + M.VLDL.FC + S.VLDL.FC + Total.L + Omega.6 + HDL.TG + M.LDL.P,
+  data=ds_metabolome, proximity=TRUE)
 ## Print model
 model
 
@@ -54,22 +70,22 @@ oob.error.data <- data.frame(
 
 ggplot(data=oob.error.data, aes(x=Trees, y=Error)) + geom_line(aes(color=Type))
 
-model <- randomForest(Status_Fractures ~ ., data=ds.imputed, ntree=1000, proximity=TRUE)
-model
-
-oob.error.data <- data.frame(
-  Trees=rep(1:nrow(model$err.rate), times=3),
-  Type=rep(c("OOB", "Yes", "No"), each=nrow(model$err.rate)),
-  Error=c(model$err.rate[,"OOB"],
-    model$err.rate[,"Yes"],
-    model$err.rate[,"No"]))
-
-ggplot(data=oob.error.data, aes(x=Trees, y=Error)) + geom_line(aes(color=Type))
+# model <- randomForest(Status_Fractures ~ ., data=ds.imputed, ntree=1000, proximity=TRUE)
+# model
+#
+# oob.error.data <- data.frame(
+#   Trees=rep(1:nrow(model$err.rate), times=3),
+#   Type=rep(c("OOB", "Yes", "No"), each=nrow(model$err.rate)),
+#   Error=c(model$err.rate[,"OOB"],
+#     model$err.rate[,"Yes"],
+#     model$err.rate[,"No"]))
+#
+# ggplot(data=oob.error.data, aes(x=Trees, y=Error)) + geom_line(aes(color=Type))
 
 
 oob.values <- vector(length=100)
 for(i in 1:100) {
-  temp.model <- randomForest(Status_Fractures ~ ., data=ds.imputed, mtry=i, ntree=1000)
+  temp.model <- randomForest(Status_Fractures ~ ., data=ds_metabolome, mtry=i, ntree=1000)
   oob.values[i] <- temp.model$err.rate[nrow(temp.model$err.rate),1]
 }
 oob.values
@@ -78,7 +94,7 @@ min(oob.values)
 ## find the optimal value for mtry...
 which(oob.values == min(oob.values))
 ## create a model for proximities using the best value for mtry
-model <- randomForest(Status_Fractures ~ ., data=ds.imputed, ntree=1000, proximity=TRUE, mtry=which(oob.values == min(oob.values)))
+model <- randomForest(Status_Fractures ~ ., data=ds_metabolome, ntree=1000, proximity=TRUE, mtry=which(oob.values == min(oob.values)))
 model
 
 ## Start by converting the proximity matrix into a distance matrix.
@@ -91,7 +107,7 @@ mds.var.per <- round(mds.stuff$eig/sum(mds.stuff$eig)*100, 1)
 
 ## now make a fancy looking plot that shows the MDS axes and the variation:
 mds.values <- mds.stuff$points
-mds.data <- data.frame(Sample=rownames(mds.values), X=mds.values[,1], Y=mds.values[,2], Status=ds.imputed$Status_Fractures)
+mds.data <- data.frame(Sample=rownames(mds.values), X=mds.values[,1], Y=mds.values[,2], Status=ds_metabolome$Status_Fractures)
 
 ggplot(data=mds.data, aes(x=X, y=Y, label=Sample)) +
   geom_text(aes(color=Status)) +
@@ -119,17 +135,24 @@ str(ds)
 ## Start building a random forest
 set.seed(42)
 
-## Impute any missing values in the training set using proximities
-ds.imputed <- rfImpute(Status_BMD ~ ., data = ds, iter=6)
+## Impute any missing values in the training set using proximities - No need to impute for Males
+# ds.imputed <- rfImpute(Status_BMD ~ ., data = ds, iter=6)
 
 ## Get random forest model with Status Fractures as the response variable
-model <- randomForest(Status_BMD ~ ., data=ds.imputed, proximity=TRUE)
+#model <- randomForest(Status_BMD ~ ., data=ds.imputed, proximity=TRUE)
+model <- randomForest(
+  Status_BMD ~ Sphingomyelins + Gly + M.VLDL.FC + S.LDL.P + Total.C + S.LDL.CE + LDL.P + M.VLDL.CE + M.VLDL.C + L.LDL.P + ApoB + S.LDL.L +
+    S.LDL.PL + Glycerol + Glucose + S.LDL.C + non.HDL.C + L.LDL.CE + IDL.P + S.VLDL.FC + L.LDL.C + M.VLDL.PL + L.LDL.FC + Total.CE +
+    Clinical.LDL.C + HDLsize + M.LDL.P + His + Total.L + L.LDL.L + IDL.FC + LDL.CE + LDL.L + LDL.C + M.LDL.CE + LDL.PL + L.LDL.PL +
+    M.LDL.L + M.VLDL.P,
+  data=ds_metabolome, proximity=TRUE)
+
 ## Print model
 model
 
 ## Check if the random forest is big enough.
 oob.error.data <- data.frame(
-  Trees=rep(1:nrow(model$err.rate), times=4),
+  Trees=rep(seq_len(nrow(model$err.rate)), times=4),
   Type=rep(c("OOB", "Normal", "Osteopenia", "Osteoporosis"), each=nrow(model$err.rate)),
   Error=c(model$err.rate[,"OOB"],
     model$err.rate[,"Normal"],
@@ -139,11 +162,11 @@ oob.error.data <- data.frame(
 ggplot(data=oob.error.data, aes(x=Trees, y=Error)) +
   geom_line(aes(color=Type))
 
-model <- randomForest(Status_BMD ~ ., data=ds.imputed, ntree=200, proximity=TRUE)
+model <- randomForest(Status_BMD ~ ., data=ds_metabolome, ntree=200, proximity=TRUE)
 model
 
 oob.error.data <- data.frame(
-  Trees=rep(1:nrow(model$err.rate), times=4),
+  Trees=rep(seq_len(nrow(model$err.rate)), times=4),
   Type=rep(c("OOB", "Normal", "Osteopenia", "Osteoporosis"), each=nrow(model$err.rate)),
   Error=c(model$err.rate[,"OOB"],
     model$err.rate[,"Normal"],
@@ -155,7 +178,7 @@ ggplot(data=oob.error.data, aes(x=Trees, y=Error)) + geom_line(aes(color=Type))
 
 oob.values <- vector(length=10)
 for(i in 1:10) {
-  temp.model <- randomForest(Status_BMD ~ ., data=ds.imputed, mtry=i, ntree=1000)
+  temp.model <- randomForest(Status_BMD ~ ., data=ds_metabolome, mtry=i, ntree=1000)
   oob.values[i] <- temp.model$err.rate[nrow(temp.model$err.rate),1]
 }
 oob.values
@@ -164,7 +187,7 @@ min(oob.values)
 ## find the optimal value for mtry...
 which(oob.values == min(oob.values))
 ## create a model for proximities using the best value for mtry
-model <- randomForest(Status_BMD ~ ., data=ds.imputed, ntree=1000, proximity=TRUE, mtry=which(oob.values == min(oob.values)))
+model <- randomForest(Status_BMD ~ ., data=ds_metabolome, ntree=1000, proximity=TRUE, mtry=which(oob.values == min(oob.values)))
 model
 
 ## Start by converting the proximity matrix into a distance matrix.
@@ -177,7 +200,7 @@ mds.var.per <- round(mds.stuff$eig/sum(mds.stuff$eig)*100, 1)
 
 ## now make a fancy looking plot that shows the MDS axes and the variation:
 mds.values <- mds.stuff$points
-mds.data <- data.frame(Sample=rownames(mds.values), X=mds.values[,1], Y=mds.values[,2], Status=ds.imputed$Status_BMD)
+mds.data <- data.frame(Sample=rownames(mds.values), X=mds.values[,1], Y=mds.values[,2], Status=ds_metabolome$Status_BMD)
 
 ggplot(data=mds.data, aes(x=X, y=Y, label=Sample)) +
   geom_text(aes(color=Status)) +
